8903290e24d254b508813f0ce2a8b44b39907647
hijazi101 <arabickalemat@gmail.com>
2024-07-19 12:03:09 +0300

    Update firebaseconfig.js

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,10 +1,8 @@
-// Import the necessary Firebase modules
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
-// Firebase configuration object
 const firebaseConfig = {
   apiKey: "AIzaSyBYqdtLRho0kcUg2mf4rjt0i26SwkQ0Uro",
   authDomain: "hijazistore.firebaseapp.com",
@@ -15,11 +13,9 @@ const firebaseConfig = {
   measurementId: "G-YZZEYG0RZ1"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
 const auth = getAuth(app);
 
-// Export Firebase modules
 export { app, db, storage, auth };
