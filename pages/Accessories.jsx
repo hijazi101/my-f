be89dc24efbae592e3411b23b55135835de852c8
hijazi101 <arabickalemat@gmail.com>
2024-07-20 14:33:16 +0300

    Update Accessories.jsx

--- pages/Accessories.jsx
+++ pages/Accessories.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { useUser } from '@clerk/nextjs';
 import { getFirestore, collection, getDocs, doc, deleteDoc, updateDoc, query, where } from 'firebase/firestore';
-import app from '../firebaseconfig';
+import { app} from '../firebaseconfig';
 import Nav from '../components/Nav';
 import Toast from '../components/Toast';
 
