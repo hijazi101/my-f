6e7570f08fe1c0e662855f78dfccfb1093921904
hijazi101 <arabickalemat@gmail.com>
2024-07-20 14:34:49 +0300

    Update Tablet.jsx

--- pages/Tablet.jsx
+++ pages/Tablet.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useUser } from '@clerk/nextjs';
 import { getFirestore, collection, getDocs, doc, deleteDoc, updateDoc, query, where } from 'firebase/firestore';
-import app from '../firebaseconfig';
+import { app} from '../firebaseconfig';
 import Nav from '../components/Nav';
 import { useRouter } from 'next/router';
 
