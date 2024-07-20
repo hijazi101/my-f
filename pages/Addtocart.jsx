ac16a3bd365cc5f23cb1db3933a26c696400fc97
hijazi101 <arabickalemat@gmail.com>
2024-07-20 14:33:47 +0300

    Update Addtocart.jsx

--- pages/Addtocart.jsx
+++ pages/Addtocart.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { SignIn, useUser, RedirectToSignIn } from '@clerk/nextjs';
-import app from '../firebaseconfig';
+import { app} from '../firebaseconfig';
 import { doc, getFirestore, setDoc } from 'firebase/firestore';
 import Nav from '../components/Nav';
 
